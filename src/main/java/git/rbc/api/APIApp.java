466ffb4a1ae8dd3f21c027a68c691788bdb3dc43
Ramesh Chipilla <rameshchipilla@users.noreply.github.com>
2021-05-26 23:18:45 -0700

    added SOP

--- src/main/java/git/rbc/api/APIApp.java
+++ src/main/java/git/rbc/api/APIApp.java
@@ -13,6 +13,7 @@ public class APIApp {
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(APIApp.class);
 //		app.setDefaultProperties(Collections.singletonMap("server.port", "9092"));
+		System.out.println("Testing auto deploy");
 		app.run(args);
 	}
 
